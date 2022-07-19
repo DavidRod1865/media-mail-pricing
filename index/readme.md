5564ab27f1831baba7f37c17e514f89e917d361a
David Rodriguez <David.Rod1865@gmail.com>
2022-07-18 21:55:55 -0400

    Created Repo for Media Mail Website and uploaded HTML & JS

---
+++ index/readme.md
@@ -0,0 +1,22 @@
+I built a calculator that will calculate and all-in price for a book that you wish to sell domestically. Based on the information that the user inputs, the user will get a total price including USPS Media Mail shipping fees and PayPal fees if the the user wishes to make a transaction using PayPal's "Goods & Services" service. Note that this only for use to calculate costs for shipping within the Domestic US only.
+
+The user will need to input the following values in order for the calculator to work.
+
+1. Value of the Book/Books - this is the value of the book being shipped. The value is determined by the user. "BookSwap" does not determine nor influence the value determined by the user.
+
+2. Weight of the Book / Package - this is the weight of the total package after packing the books. In order to qualify for Media Mail, the weight cannot exceed 70 lbs per individual package. Note: Media Mail prices are based on the weight of the piece WITHOUT REGARD TO ZONE. Weights must be accurate for an accurate estimate.
+
+Once the above values have been entered, the calculator will run and give you two prices. One price calculating amount you will get after shipping fees and Paypal fees are taken out. A second price calculating the amount you need to sell you book at in order to get the value you want for the book.
+
+Price One Example:
+Value of Book: $200.00
+Shipping: $10.00 (Taken Out)
+PayPal Fee: $5.00 (Taken Out)
+"If you sell the book at $200.00, you will receive $185.00 after shipping and fees."
+
+Price Two Example:
+Value of Book: $200.00
+Shipping: $10.00 (Added In)
+PayPal Fee: $5.00 (Added In)
+"If you sell the book at $215.00, you will receive $200.00 after shipping and fees."
+
---
+++ index/script.js
@@ -0,0 +1,332 @@
+function calShipPay(){
+    let weight = document.getElementById("weight").value;  
+        if (weight == 0){
+            alert("Enter Value Greater Than 0!"); return false;
+        } else if (weight == 1){
+            document.querySelector(".shippingFee1").textContent = "$3.19"; return false; 
+        } else if (weight == 2){
+            document.querySelector(".shippingFee1").textContent = "$3.82"; return false;
+        } else if (weight == 3){
+            document.querySelector(".shippingFee1").textContent = "$4.45"; return false;
+        } else if (weight == 4){
+            document.querySelector(".shippingFee1").textContent = "$5.08"; return false;
+        } else if (weight == 5){
+            document.querySelector(".shippingFee1").textContent = "$5.71"; return false;
+        } else if (weight == 6){
+            document.querySelector(".shippingFee1").textContent = "$6.34"; return false;
+        } else if (weight == 7){
+            document.querySelector(".shippingFee1").textContent = "$6.97"; return false;
+        } else if (weight == 8){
+            document.querySelector(".shippingFee1").textContent = "$7.60"; return false;
+        } else if (weight == 9){
+            document.querySelector(".shippingFee1").textContent = "$8.23"; return false;
+        } else if (weight == 10){
+            document.querySelector(".shippingFee1").textContent = "$8.86"; return false;
+        } else if (weight == 11){
+            document.querySelector(".shippingFee1").textContent = "$9.49"; return false;
+        } else if (weight == 12){
+            document.querySelector(".shippingFee1").textContent = "$10.12"; return false;
+        } else if (weight == 13){
+            document.querySelector(".shippingFee1").textContent = "$10.75"; return false;
+        } else if (weight == 14){
+            document.querySelector(".shippingFee1").textContent = "$11.38"; return false;
+        } else if (weight == 15){
+            document.querySelector(".shippingFee1").textContent = "$12.01"; return false;
+        } else if (weight == 16){
+            document.querySelector(".shippingFee1").textContent = "$12.64"; return false;
+        } else if (weight == 17){
+            document.querySelector(".shippingFee1").textContent = "$13.27"; return false;
+        } else if (weight == 18){
+            document.querySelector(".shippingFee1").textContent = "$13.90"; return false;
+        } else if (weight == 19){
+            document.querySelector(".shippingFee1").textContent = "$14.53"; return false;
+        } else if (weight == 20){
+            document.querySelector(".shippingFee1").textContent = "$15.16"; return false;
+        } else if (weight == 21){
+            document.querySelector(".shippingFee1").textContent = "$15.79"; return false;
+        } else if (weight == 22){
+            document.querySelector(".shippingFee1").textContent = "$16.42"; return false;
+        } else if (weight == 24){
+            document.querySelector(".shippingFee1").textContent = "$17.68"; return false;
+        } else if (weight == 25){
+            document.querySelector(".shippingFee1").textContent = "$18.31"; return false;
+        } else if (weight == 26){
+            document.querySelector(".shippingFee1").textContent = "$18.94"; return false;
+        } else if (weight == 27){
+            document.querySelector(".shippingFee1").textContent = "$19.57"; return false;
+        } else if (weight == 28){
+            document.querySelector(".shippingFee1").textContent = "$20.20"; return false;
+        } else if (weight == 29){
+            document.querySelector(".shippingFee1").textContent = "$20.83"; return false;
+        } else if (weight == 30){
+            document.querySelector(".shippingFee1").textContent = "$21.83"; return false;
+        } else if (weight == 31){
+            document.querySelector(".shippingFee1").textContent = "$22.09"; return false;
+        } else if (weight == 32){
+            document.querySelector(".shippingFee1").textContent = "$22.72"; return false;
+        } else if (weight == 33){
+            document.querySelector(".shippingFee1").textContent = "$23.25"; return false;
+        } else if (weight == 34){
+            document.querySelector(".shippingFee1").textContent = "$23.98"; return false;
+        } else if (weight == 35){
+            document.querySelector(".shippingFee1").textContent = "$24.61"; return false;
+        } else if (weight == 36){
+            document.querySelector(".shippingFee1").textContent = "$25.24"; return false;
+        } else if (weight == 37){
+            document.querySelector(".shippingFee1").textContent = "$25.87"; return false;
+        } else if (weight == 38){
+            document.querySelector(".shippingFee1").textContent = "$26.50"; return false;
+        } else if (weight == 39){
+            document.querySelector(".shippingFee1").textContent = "$27.13"; return false;
+        } else if (weight == 40){
+            document.querySelector(".shippingFee1").textContent = "$27.76"; return false;
+        } else if (weight == 41){
+            document.querySelector(".shippingFee1").textContent = "$28.39"; return false;
+        } else if (weight == 42){
+            document.querySelector(".shippingFee1").textContent = "$29.01"; return false;
+        } else if (weight == 43){
+            document.querySelector(".shippingFee1").textContent = "$29.65"; return false;
+        } else if (weight == 44){
+            document.querySelector(".shippingFee1").textContent = "$30.28"; return false;
+        } else if (weight == 45){
+            document.querySelector(".shippingFee1").textContent = "$30.91"; return false;
+        } else if (weight == 46){
+            document.querySelector(".shippingFee1").textContent = "$31.54"; return false;
+        } else if (weight == 47){
+            document.querySelector(".shippingFee1").textContent = "$32.17"; return false;
+        } else if (weight == 48){
+            document.querySelector(".shippingFee1").textContent = "$32.80"; return false;
+        } else if (weight == 49){
+            document.querySelector(".shippingFee1").textContent = "$33.43"; return false;
+        } else if (weight == 50){
+            document.querySelector(".shippingFee1").textContent = "$34.06"; return false;
+        } else if (weight == 51){
+            document.querySelector(".shippingFee1").textContent = "$34.69"; return false;
+        } else if (weight == 52){
+            document.querySelector(".shippingFee1").textContent = "$35.52"; return false;
+        } else if (weight == 53){
+            document.querySelector(".shippingFee1").textContent = "$35.95"; return false;
+        } else if (weight == 54){
+            document.querySelector(".shippingFee1").textContent = "$36.58"; return false;
+        } else if (weight == 55){
+            document.querySelector(".shippingFee1").textContent = "$37.21"; return false;
+        } else if (weight == 56){
+            document.querySelector(".shippingFee1").textContent = "$37.84"; return false;
+        } else if (weight == 57){
+            document.querySelector(".shippingFee1").textContent = "$38.47"; return false;
+        } else if (weight == 58){
+            document.querySelector(".shippingFee1").textContent = "$39.10"; return false;
+        } else if (weight == 59){
+            document.querySelector(".shippingFee1").textContent = "$39.73"; return false;
+        } else if (weight == 60){
+            document.querySelector(".shippingFee1").textContent = "$40.36"; return false;
+        } else if (weight == 61){
+            document.querySelector(".shippingFee1").textContent = "$40.99"; return false;
+        } else if (weight == 62){
+            document.querySelector(".shippingFee1").textContent = "$41.62"; return false;
+        } else if (weight == 63){
+            document.querySelector(".shippingFee1").textContent = "$42.25"; return false;
+        } else if (weight == 64){
+            document.querySelector(".shippingFee1").textContent = "$42.88"; return false;
+        } else if (weight == 65){
+            document.querySelector(".shippingFee1").textContent = "$43.51"; return false;
+        } else if (weight == 66){
+            document.querySelector(".shippingFee1").textContent = "$44.14"; return false;
+        } else if (weight == 67){
+            document.querySelector(".shippingFee1").textContent = "$44.77"; return false;
+        } else if (weight == 68){
+            document.querySelector(".shippingFee1").textContent = "$45.40"; return false;
+        } else if (weight == 69){
+            document.querySelector(".shippingFee1").textContent = "$46.03"; return false;
+        } else if (weight == 70){
+            document.querySelector(".shippingFee1").textContent = "$46.66"; return false;
+        } else if (weight > 70){
+            alert("Weight Must Be Less Than 70 Lbs."); return false;
+        }
+    };
+
+function calShipPay2(){
+    let weight = document.getElementById("weight").value;  
+        if (weight == 0){
+            alert("Enter Value Greater Than 0!"); return false;
+        } else if (weight == 1){
+            document.querySelector(".shippingFee2").textContent = "$3.19"; return false; 
+        } else if (weight == 2){
+            document.querySelector(".shippingFee2").textContent = "$3.82"; return false;
+        } else if (weight == 3){
+            document.querySelector(".shippingFee2").textContent = "$4.45"; return false;
+        } else if (weight == 4){
+            document.querySelector(".shippingFee2").textContent = "$5.08"; return false;
+        } else if (weight == 5){
+            document.querySelector(".shippingFee2").textContent = "$5.71"; return false;
+        } else if (weight == 6){
+            document.querySelector(".shippingFee2").textContent = "$6.34"; return false;
+        } else if (weight == 7){
+            document.querySelector(".shippingFee2").textContent = "$6.97"; return false;
+        } else if (weight == 8){
+            document.querySelector(".shippingFee2").textContent = "$7.60"; return false;
+        } else if (weight == 9){
+            document.querySelector(".shippingFee2").textContent = "$8.23"; return false;
+        } else if (weight == 10){
+            document.querySelector(".shippingFee2").textContent = "$8.86"; return false;
+        } else if (weight == 11){
+            document.querySelector(".shippingFee2").textContent = "$9.49"; return false;
+        } else if (weight == 12){
+            document.querySelector(".shippingFee2").textContent = "$10.12"; return false;
+        } else if (weight == 13){
+            document.querySelector(".shippingFee2").textContent = "$10.75"; return false;
+        } else if (weight == 14){
+            document.querySelector(".shippingFee2").textContent = "$11.38"; return false;
+        } else if (weight == 15){
+            document.querySelector(".shippingFee2").textContent = "$12.01"; return false;
+        } else if (weight == 16){
+            document.querySelector(".shippingFee2").textContent = "$12.64"; return false;
+        } else if (weight == 17){
+            document.querySelector(".shippingFee2").textContent = "$13.27"; return false;
+        } else if (weight == 18){
+            document.querySelector(".shippingFee2").textContent = "$13.90"; return false;
+        } else if (weight == 19){
+            document.querySelector(".shippingFee2").textContent = "$14.53"; return false;
+        } else if (weight == 20){
+            document.querySelector(".shippingFee2").textContent = "$15.16"; return false;
+        } else if (weight == 21){
+            document.querySelector(".shippingFee2").textContent = "$15.79"; return false;
+        } else if (weight == 22){
+            document.querySelector(".shippingFee2").textContent = "$16.42"; return false;
+        } else if (weight == 24){
+            document.querySelector(".shippingFee2").textContent = "$17.68"; return false;
+        } else if (weight == 25){
+            document.querySelector(".shippingFee2").textContent = "$18.31"; return false;
+        } else if (weight == 26){
+            document.querySelector(".shippingFee2").textContent = "$18.94"; return false;
+        } else if (weight == 27){
+            document.querySelector(".shippingFee2").textContent = "$19.57"; return false;
+        } else if (weight == 28){
+            document.querySelector(".shippingFee2").textContent = "$20.20"; return false;
+        } else if (weight == 29){
+            document.querySelector(".shippingFee2").textContent = "$20.83"; return false;
+        } else if (weight == 30){
+            document.querySelector(".shippingFee2").textContent = "$21.83"; return false;
+        } else if (weight == 31){
+            document.querySelector(".shippingFee2").textContent = "$22.09"; return false;
+        } else if (weight == 32){
+            document.querySelector(".shippingFee2").textContent = "$22.72"; return false;
+        } else if (weight == 33){
+            document.querySelector(".shippingFee2").textContent = "$23.25"; return false;
+        } else if (weight == 34){
+            document.querySelector(".shippingFee2").textContent = "$23.98"; return false;
+        } else if (weight == 35){
+            document.querySelector(".shippingFee2").textContent = "$24.61"; return false;
+        } else if (weight == 36){
+            document.querySelector(".shippingFee2").textContent = "$25.24"; return false;
+        } else if (weight == 37){
+            document.querySelector(".shippingFee2").textContent = "$25.87"; return false;
+        } else if (weight == 38){
+            document.querySelector(".shippingFee2").textContent = "$26.50"; return false;
+        } else if (weight == 39){
+            document.querySelector(".shippingFee2").textContent = "$27.13"; return false;
+        } else if (weight == 40){
+            document.querySelector(".shippingFee2").textContent = "$27.76"; return false;
+        } else if (weight == 41){
+            document.querySelector(".shippingFee2").textContent = "$28.39"; return false;
+        } else if (weight == 42){
+            document.querySelector(".shippingFee2").textContent = "$29.01"; return false;
+        } else if (weight == 43){
+            document.querySelector(".shippingFee2").textContent = "$29.65"; return false;
+        } else if (weight == 44){
+            document.querySelector(".shippingFee2").textContent = "$30.28"; return false;
+        } else if (weight == 45){
+            document.querySelector(".shippingFee2").textContent = "$30.91"; return false;
+        } else if (weight == 46){
+            document.querySelector(".shippingFee2").textContent = "$31.54"; return false;
+        } else if (weight == 47){
+            document.querySelector(".shippingFee2").textContent = "$32.17"; return false;
+        } else if (weight == 48){
+            document.querySelector(".shippingFee2").textContent = "$32.80"; return false;
+        } else if (weight == 49){
+            document.querySelector(".shippingFee2").textContent = "$33.43"; return false;
+        } else if (weight == 50){
+            document.querySelector(".shippingFee2").textContent = "$34.06"; return false;
+        } else if (weight == 51){
+            document.querySelector(".shippingFee2").textContent = "$34.69"; return false;
+        } else if (weight == 52){
+            document.querySelector(".shippingFee2").textContent = "$35.52"; return false;
+        } else if (weight == 53){
+            document.querySelector(".shippingFee2").textContent = "$35.95"; return false;
+        } else if (weight == 54){
+            document.querySelector(".shippingFee2").textContent = "$36.58"; return false;
+        } else if (weight == 55){
+            document.querySelector(".shippingFee2").textContent = "$37.21"; return false;
+        } else if (weight == 56){
+            document.querySelector(".shippingFee2").textContent = "$37.84"; return false;
+        } else if (weight == 57){
+            document.querySelector(".shippingFee2").textContent = "$38.47"; return false;
+        } else if (weight == 58){
+            document.querySelector(".shippingFee2").textContent = "$39.10"; return false;
+        } else if (weight == 59){
+            document.querySelector(".shippingFee2").textContent = "$39.73"; return false;
+        } else if (weight == 60){
+            document.querySelector(".shippingFee2").textContent = "$40.36"; return false;
+        } else if (weight == 61){
+            document.querySelector(".shippingFee2").textContent = "$40.99"; return false;
+        } else if (weight == 62){
+            document.querySelector(".shippingFee2").textContent = "$41.62"; return false;
+        } else if (weight == 63){
+            document.querySelector(".shippingFee2").textContent = "$42.25"; return false;
+        } else if (weight == 64){
+            document.querySelector(".shippingFee2").textContent = "$42.88"; return false;
+        } else if (weight == 65){
+            document.querySelector(".shippingFee2").textContent = "$43.51"; return false;
+        } else if (weight == 66){
+            document.querySelector(".shippingFee2").textContent = "$44.14"; return false;
+        } else if (weight == 67){
+            document.querySelector(".shippingFee2").textContent = "$44.77"; return false;
+        } else if (weight == 68){
+            document.querySelector(".shippingFee2").textContent = "$45.40"; return false;
+        } else if (weight == 69){
+            document.querySelector(".shippingFee2").textContent = "$46.03"; return false;
+        } else if (weight == 70){
+            document.querySelector(".shippingFee2").textContent = "$46.66"; return false;
+        } else if (weight > 70){
+            alert("Weight Must Be Less Than 70 Lbs."); return false;
+        }
+    };
+    
+function paypal(){
+    let paypalFee = document.getElementById("value").value;  
+    if (paypalFee > 0){
+        const paypalFeeResult = (paypalFee * .0349) + 0.49;
+        const paypalFeeNew = Number(paypalFeeResult.toFixed(2))
+        document.querySelector(".paypalFee1").textContent = paypalFeeNew; 
+        return false;
+        } else if (paypalFee <= 0){
+            document.querySelector(".paypalFee1").textContent = "$0.00"; 
+            return false;
+        }
+    }
+
+function paypal2(){
+    let paypalFee = document.getElementById("value").value;  
+    if (paypalFee > 0){
+        const paypalFeeResult = (paypalFee * .0349) + 0.49;
+        const paypalFeeNew = Number(paypalFeeResult.toFixed(2))
+        document.querySelector(".paypalFee2").textContent = paypalFeeNew; 
+        return false;
+        } else if (paypalFee <= 0){
+            document.querySelector(".paypalFee2").textContent = "$0.00"; 
+            return false;
+        }
+    }
+
+let calculate = document.querySelector("button").addEventListener('click', () => {
+    calShipPay();
+    paypal();
+    calShipPay2();
+    paypal2();
+    event.preventDefault();
+    });
+
+function afterFees(){
+    let value = document.getElementById("value").value;
+    document.querySelector('.userValue').textContent = value;
+}
